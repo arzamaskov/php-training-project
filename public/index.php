160d975ba181032741b64a1b07d14dde7f8ada1c
Andrey Arzamaskov <arzamaskov@gmail.com>
2021-03-11 23:25:31 +0400

    Modify index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
 <?php
 
-echo __FILE__;
+$query = rtrim($_SERVER['QUERY_STRING'], '/');
+
+require_once '../vendor/core/Router.php';
+require_once '../vendor/libs/functions.php';
+
+Router::add('posts.add', ['controller' => 'Posts', 'mode' => 'add']);
+Router::add('posts', ['controller' => 'Posts', 'mode' => 'index']);
+Router::add('', ['controller' => 'Main', 'mode' => 'index']);
+
+debug(Router::getRouteList());
+
+if (Router::matchRoute($query)) {
+    debug(Router::getRoute());
+} else {
+    echo '404';
+}
