2098b5171af4f5eff69d0c4f69ef44dbdde02199
Andrey Arzamaskov <arzamaskov@gmail.com>
2021-03-11 23:28:18 +0400

    Create methods add, getRouteList, getRoute and matchRoute in class Router

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -2,7 +2,41 @@
 
 class Router
 {
-    public function __construct() {
-        echo 'Hello, World!';
+    /*
+     * Table of routes
+     */
+    protected static $route_list = [];
+
+    /*
+     * Current route
+     */
+    protected static $route = [];
+
+    public static function add($regexp, $route = [])
+    {
+        self::$route_list[$regexp] = $route;
+    }
+
+    public static function getRouteList()
+    {
+        return self::$route_list;
+    }
+
+    public static function getRoute()
+    {
+        return self::$route;
+    }
+
+    public static function matchRoute($url)
+    {
+        foreach (self::$route_list as $pattern => $route) {
+            if ($pattern == $url) {
+                self::$route = $route;
+
+                return true;
+            }
+        }
+
+        return false;
     }
 }
