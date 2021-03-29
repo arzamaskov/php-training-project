6b9a433c752f491a5a383f238cb4c81265630aae
Andrey Arzamaskov <aarzamaskov@simtechdev.org>
2021-03-29 17:51:37 +0400

    Modify regexps

--- public/index.php
+++ public/index.php
@@ -5,14 +5,10 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 require_once '../vendor/core/Router.php';
 require_once '../vendor/libs/functions.php';
 
-Router::add('posts.add', ['controller' => 'Posts', 'mode' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'mode' => 'index']);
-Router::add('', ['controller' => 'Main', 'mode' => 'index']);
+Router::add('^$', ['controller' => 'Main', 'mode' => 'index']);
+Router::add('^(?P<controller>[a-z-]+).?(?P<mode>[a-z-]+)?$', []);
 
 debug(Router::getRouteList());
 
-if (Router::matchRoute($query)) {
-    debug(Router::getRoute());
-} else {
-    echo '404';
-}
+Router::dispatch($query);
+
