5181f7f851cac9cfa638260855461b49b2284d84
Andrey Arzamaskov <arzamaskov@gmail.com>
2021-03-30 18:04:16 +0400

    Modify index.php

--- public/index.php
+++ public/index.php
@@ -4,11 +4,14 @@ $query = rtrim($_SERVER['QUERY_STRING'], '/');
 
 require_once '../vendor/core/Router.php';
 require_once '../vendor/libs/functions.php';
+require_once '../app/controllers/Main.php';
+require_once '../app/controllers/Posts.php';
+require_once '../app/controllers/PostsNew.php';
 
 Router::add('^$', ['controller' => 'Main', 'mode' => 'index']);
 Router::add('^(?P<controller>[a-z-]+).?(?P<mode>[a-z-]+)?$', []);
 
-debug(Router::getRouteList());
+/* debug(Router::getRouteList()); */
 
 Router::dispatch($query);
 
